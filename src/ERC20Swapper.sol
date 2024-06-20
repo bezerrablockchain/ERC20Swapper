eea3881894f44af6aaade5e42537760a84f1931d
bezerrablockchain <bezerrablockchain@gmail.com>
2024-06-20 15:17:05 -0400

    tests: Adding Unit test with fork

--- src/ERC20Swapper.sol
+++ src/ERC20Swapper.sol
@@ -1,12 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
 import "./IERC20Swapper.sol";
 
-contract ERC20Swapper is IERC20Swapper, UUPSUpgradeable, OwnableUpgradeable {
+contract ERC20Swapper is
+    IERC20Swapper,
+    UUPSUpgradeable,
+    OwnableUpgradeable,
+    ReentrancyGuardUpgradeable
+{
     struct ExactInputSingleParams {
         address tokenIn;
         address tokenOut;
@@ -16,54 +23,95 @@ contract ERC20Swapper is IERC20Swapper, UUPSUpgradeable, OwnableUpgradeable {
         uint256 amountOutMinimum;
         uint160 sqrtPriceLimitX96;
     }
+
+    event TokenSwapped(
+        address indexed tokenAddress,
+        uint256 indexed amountIn,
+        uint256 indexed amountOut,
+        uint minAmount
+    );
+
     error FailedToSwap();
     error MinValueNotReached();
+    error NonZeroValueAllowed();
+
+    address constant SWAP_ROUTER_ADDR =
+        0xE592427A0AEce92De3Edee1F18E0157C05861564; //Polygon
+    address constant WETH9 = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619; //Polygon
+    address constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270; //Polygon
+    uint24 constant POOL_FEE = 3000;
 
     function initialize() public initializer {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
     }
 
-    /// @dev swaps the `msg.value` Ether to at least `minAmount` of tokens in `address`, or reverts
-    /// @param token The address of ERC-20 token to swap
-    /// @param minAmount The minimum amount of tokens transferred to msg.sender
-    /// @return amountOut The actual amount of transferred tokens
     function swapEtherToToken(
         address token,
         uint minAmount
     ) external payable returns (uint amountOut) {
-        address swapRouter = 0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E;
-        address WETH9 = 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14;
-        uint24 poolFee = 3000;
+        if (msg.value == 0) {
+            revert NonZeroValueAllowed();
+        }
 
-        ExactInputSingleParams memory params = ExactInputSingleParams({
-            tokenIn: WETH9,
-            tokenOut: token,
-            fee: poolFee,
-            recipient: msg.sender,
-            amountIn: msg.value,
-            amountOutMinimum: minAmount,
-            sqrtPriceLimitX96: 0
-        });
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                // tokenIn: swapRouter.WETH9(),
+                tokenIn: WMATIC,
+                tokenOut: token,
+                fee: POOL_FEE,
+                recipient: msg.sender,
+                deadline: block.timestamp + 1500,
+                amountIn: msg.value,
+                amountOutMinimum: minAmount,
+                sqrtPriceLimitX96: 0
+            });
 
-        bytes memory message = abi.encodeWithSignature(
-            "exactInputSingle((address,address,uint24,address,uint256,uint256,uint160))",
-            params
-        );
-        (bool sent, bytes memory data) = address(swapRouter).call{
-            value: msg.value
-        }(message);
+        uint amountOut = swapRouter.exactInputSingle{value: msg.value}(params);
+        if (amountOut < minAmount) revert MinValueNotReached();
 
-        if(!sent) revert FailedToSwap();
-
-        amountOut = uint256(bytes32(data));
-
-        if(amountOut < minAmount) revert MinValueNotReached();
-
+        emit TokenSwapped(token, msg.value, amountOut, minAmount);
         return amountOut;
     }
 
+    // // @dev swaps the `msg.value` Ether to at least `minAmount` of tokens in `address`, or reverts
+    // // @param token The address of ERC-20 token to swap
+    // // @param minAmount The minimum amount of tokens transferred to msg.sender
+    // // @return amountOut The actual amount of transferred tokens
+    // function swapEtherToToken2(
+    //     address token,
+    //     uint minAmount
+    // ) external payable returns (uint amountOut) {
+
+    //     ExactInputSingleParams memory params = ExactInputSingleParams({
+    //         tokenIn: WETH9,
+    //         tokenOut: token,
+    //         fee: POOL_FEE,
+    //         recipient: msg.sender,
+    //         amountIn: msg.value,
+    //         amountOutMinimum: minAmount,
+    //         sqrtPriceLimitX96: 0
+    //     });
+
+    //     bytes memory message = abi.encodeWithSignature(
+    //         "exactInputSingle((address,address,uint24,address,uint256,uint256,uint160))",
+    //         params
+    //     );
+    //     (bool sent, bytes memory data) = address(SWAP_ROUTER_ADDR).call{
+    //         value: msg.value
+    //     }(message);
+
+    //     if(!sent) revert FailedToSwap();
+
+    //     amountOut = uint256(bytes32(data));
+
+    //     if(amountOut < minAmount) revert MinValueNotReached();
+
+    //     return amountOut;
+    // }
+
     function _authorizeUpgrade(
         address newImplementation
-    ) internal virtual override {}
+    ) internal override onlyOwner {}
 }
--- test/ERC20SwapperTester.t.sol
+++ test/ERC20SwapperTester.t.sol
@@ -3,18 +3,32 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 import "../src/ERC20Swapper.sol";
+import "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 contract ERC20SwapperTest is Test {
     ERC20Swapper swapper;
-    address swapperProxy = 0xeA853Fa1181c28DCDE62e067923287771723a9A0; // Deployed contract address
-    address tokenOut = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984; // Token to swap to
+    address owner = address(0x777888999);
+    address user01 = address(0x123456789);
+    address tokenOut = address(0x3c499c542cEF5E3811e1192ce70d8cC03d5c3359); //USDC
 
     function setUp() public {
-        swapper = ERC20Swapper(swapperProxy);
+        uint256 forkId = vm.createFork(vm.envString("MAINNET_RPC_URL"));
+        vm.selectFork(forkId);
+
+        vm.deal(owner, 10 ether);
+        vm.deal(user01, 10 ether);
+
+        vm.startPrank(owner);
+        // Deploy instance and get a proxy address
+        implERC20Swapper = new ERC20Swapper();
+        proxy = new ERC1967Proxy(address(implERC20Swapper), "");
+        swapper = ERC20Swapper(address(proxy));
+        swapper.initialize();
+        vm.stopPrank();
     }
 
     function testSwapEtherToToken() public {
-        uint256 minAmount = 0 ether;
+        uint256 minAmount = 1;
 
         // This test assumes you're already connected to Sepolia and can send transactions
         // Sending Ether to the swapper contract to simulate swap
