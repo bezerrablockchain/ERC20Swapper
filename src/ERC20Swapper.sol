cc795529c7325a86dac82a149b4d91ae24ebc78d
bezerrablockchain <bezerrablockchain@gmail.com>
2024-06-22 22:38:37 -0400

    feat: fee determination

--- src/ERC20Swapper.sol
+++ src/ERC20Swapper.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
+import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
@@ -22,21 +23,28 @@ contract ERC20Swapper is
 
     error MinValueNotReached();
     error NonZeroValueAllowed();
+    error PoolNotFound();
 
     //These addresses bellow are from Polygon Mainnet
+    address constant FACTORY_ADDR = 0x1F98431c8aD98523631AE4a59f267346ea31F984;
     address constant SWAP_ROUTER_ADDR =
         0xE592427A0AEce92De3Edee1F18E0157C05861564;
-    address constant WETH9 = 0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619;
     address constant WMATIC = 0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270;
-    uint24 constant POOL_FEE = 3000;
+
+    IUniswapV3Factory public factory;
+    ISwapRouter public swapRouter;
 
     constructor() {
         // __disableInitializers();
     }
+
     function initialize() public initializer {
         __Ownable_init(msg.sender);
         __UUPSUpgradeable_init();
         __ReentrancyGuard_init();
+
+        factory = IUniswapV3Factory(FACTORY_ADDR);
+        swapRouter = ISwapRouter(SWAP_ROUTER_ADDR);
     }
 
     // @dev swaps the `msg.value` Ether to at least `minAmount` of tokens in `address`, or reverts
@@ -51,13 +59,16 @@ contract ERC20Swapper is
             revert NonZeroValueAllowed();
         }
 
-        ISwapRouter swapRouter = ISwapRouter(SWAP_ROUTER_ADDR);
+        uint24 poolFee = _getPoolFee(token);
+        if (poolFee == 0) {
+            revert PoolNotFound();
+        }
 
         ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
             .ExactInputSingleParams({
                 tokenIn: WMATIC,
                 tokenOut: token,
-                fee: POOL_FEE,
+                fee: poolFee,
                 recipient: msg.sender,
                 deadline: block.timestamp + 1500,
                 amountIn: msg.value,
@@ -70,6 +81,31 @@ contract ERC20Swapper is
 
         emit TokenSwapped(token, msg.value, amountOut, minAmount);
         return amountOut;
+    }
+
+    // @notice This function uses the most common pool fee of uniswaps pools
+    // @dev _getPoolFee returns the pool fee of the token
+    // @param token The address of ERC-20 token
+    // @return poolFee The pool fee of the token
+    function _getPoolFee(address token) internal view returns (uint24) {
+        uint24[] memory commonPoolFeeSet = new uint24[](4);
+        unchecked {
+            commonPoolFeeSet[0] = 100;
+            commonPoolFeeSet[1] = 500;
+            commonPoolFeeSet[2] = 3000;
+            commonPoolFeeSet[3] = 10000;
+        }
+        unchecked {
+            for (uint i; i < commonPoolFeeSet.length; ++i) {
+                if (
+                    factory.getPool(token, WMATIC, commonPoolFeeSet[i]) !=
+                    address(0)
+                ) {
+                    return commonPoolFeeSet[i];
+                }
+            }
+        }
+        return 0;
     }
 
     // @dev _authorizeUpgrade is used by the OpenZeppelin's UUPSUpgradeable to authorize the upgrade
